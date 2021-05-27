fb810efbbe2d74c51bf1d6a23c4f6d63149f3b02
KhomZ <mgrprinc37@gmail.com>
2021-05-27 06:17:02 +0545

    added customer.db database.db n explaination.txt

---
+++ sqlite/database.py
@@ -0,0 +1,24 @@
+# @Khom
+
+import sqlite3
+
+# conn = sqlite3.connect(':memory')
+# creating database
+conn = sqlite3.connect('customer.db')
+
+# create a cursor 
+c = conn.cursor()
+
+# Query The Database
+c.execute("SELECT * FROM customers")
+# c.fetchone()
+# c.fetchmany()
+
+print(c.fetchall())
+
+# print("Command executed succesfully...")
+# commit our command 
+conn.commit()
+
+# close our connection 
+conn.close()
